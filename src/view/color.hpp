d7bf9c4f38c055b84fe2caa01f235a6a2a363b03
MnZn <2776835685@qq.com>
2022-07-13 22:53:55 +0800

    feat: storage and operation of colors

---
+++ src/view/color.hpp
@@ -0,0 +1,46 @@
+//
+// Created by IMEI on 2022/7/13.
+//
+
+#ifndef MINI_ENGINE_COLOR_HPP
+#define MINI_ENGINE_COLOR_HPP
+
+#include <cstdint>
+
+/*
+ 提供颜色相关的api
+ */
+
+namespace mne {
+struct Color {
+    // 范围为[0,1]
+    float r, g, b; // Todo 是否考虑alpha通道?
+
+    // 颜色混合
+    friend constexpr Color operator+(const Color& lhs, const Color& rhs) {
+        return {lhs.r + rhs.r, lhs.g + rhs.g, lhs.b + rhs.b};
+    }
+
+    // 数乘
+    friend constexpr Color operator*(const Color& lhs, float mut) {
+        return {lhs.r * mut, lhs.g * mut, lhs.b * mut};
+    }
+
+    friend constexpr Color operator*(float mut, const Color& rhs) {
+        return rhs * mut;
+    }
+
+    // 数除
+    friend constexpr Color operator/(const Color& lhs, float mut) {
+        return {lhs.r / mut, lhs.g / mut, lhs.b / mut};
+    }
+
+    // 将[0,256)映射到[0,1]
+    static constexpr Color fromRGB256(std::uint8_t r, std::uint8_t g, std::uint8_t b) {
+        constexpr int div = 255;
+        return {float(r) / div, float(g) / div, float(b) / div};
+    }
+};
+} // namespace mne
+
+#endif //MINI_ENGINE_COLOR_HPP
