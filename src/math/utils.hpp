c181dfb33121b30a4c7b67adada1b753b022411b
MnZn <2776835685@qq.com>
2022-07-16 22:28:05 +0800

    feat: add <randInt> api

---
+++ src/math/utils.hpp
@@ -0,0 +1,23 @@
+﻿//
+// Created by MnZn on 2022/7/16.
+//
+
+#ifndef MINI_ENGINE_UTILS_HPP
+#define MINI_ENGINE_UTILS_HPP
+
+#include "mat.hpp"
+#include <random>
+#include <ctime>
+
+namespace mne {
+
+// [l,r)
+inline int randInt(int l, int r) {
+    static auto seed  = 0;                  // time(nullptr);
+    static auto maker = std::mt19937(seed); // NOLINT(cert-msc51-cpp)
+    return int(maker() % (r - l)) + l;
+}
+
+} // namespace mne
+
+#endif //MINI_ENGINE_UTILS_HPP
