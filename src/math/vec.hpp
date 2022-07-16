07dc3b6fce3f73cbcadd8c0648d3c17e0c825500
MnZn <2776835685@qq.com>
2022-07-16 22:27:44 +0800

    feat: add <pick> , <v_min> , <v_max> , <cdot> , <inner> , <rotate> , <non-const visitor> , <io operator> , <make_vec> api

--- src/math/vec.hpp
+++ src/math/vec.hpp
@@ -6,12 +6,16 @@
 #define MINI_ENGINE_VEC_HPP
 
 #include <cmath>
+#include <algorithm>
+#include <iostream>
 
 namespace mne {
 
 using number = float;
 
 constexpr number eps = 1e-8f;
+
+constexpr inline number pi = 3.14159265358979323846f;
 
 template<int N>
 requires(N >= 1) struct Vec {
@@ -43,12 +47,45 @@ public:
         return ret;
     }
 
+    // 挑选出某个维度的向量
+    template<int... idx>
+    constexpr Vec<sizeof...(idx)> pick() const {
+        return Vec<sizeof...(idx)>{data[idx]...};
+    }
+
+    // 最值
+    constexpr number v_min() const {
+        return *std::min_element(data, data + N);
+    }
+    constexpr number v_max() const {
+        return *std::max_element(data, data + N);
+    }
+
     // 叉积
     constexpr Vec cross(const Vec& rhs) const requires(N == 3) {
         const Vec& lhs = *this;
         return {lhs[1] * rhs[2] - lhs[2] * rhs[1],
                 lhs[2] * rhs[0] - lhs[0] * rhs[2],
                 lhs[0] * rhs[1] - lhs[1] * rhs[0]};
+    }
+
+    constexpr number cdot(const Vec& rhs) const requires(N == 2) {
+        const Vec& lhs = *this;
+        return lhs[1] * rhs[0] - lhs[0] * rhs[1];
+    }
+
+    // this和rhs的夹角, [-pi, pi]
+    constexpr number inner(const Vec& rhs) const {
+        const Vec& lhs = *this;
+        return std::acos((lhs * rhs) / (lhs.length() * rhs.length()));
+    }
+
+    // this顺时针旋转到target需要的角度
+    constexpr number rotate(const Vec& target) const {
+        const Vec &lhs = *this, &rhs = target;
+        number     angle = inner(rhs);
+        if (lhs.cdot(rhs) < 0) angle = 2 * pi - angle;
+        return angle;
     }
 #pragma endregion
 public:
@@ -57,7 +94,13 @@ public:
     constexpr number x() const requires(N >= 1) {
         return data[0];
     }
+    constexpr number& x() requires(N >= 1) {
+        return data[0];
+    }
     constexpr number y() const requires(N >= 2) {
+        return data[1];
+    }
+    constexpr number& y() requires(N >= 2) {
         return data[1];
     }
 
@@ -65,9 +108,15 @@ public:
     constexpr number z() const requires(N >= 3) {
         return data[2];
     }
+    constexpr number& z() requires(N >= 3) {
+        return data[2];
+    }
 
     // 四元数
     constexpr number w() const requires(N >= 4) {
+        return data[3];
+    }
+    constexpr number& w() requires(N >= 4) {
         return data[3];
     }
 
@@ -118,6 +167,16 @@ public:
     constexpr friend Vec operator*(number k, const Vec& rhs) { return rhs * k; }
 
     constexpr friend Vec operator/(const Vec& lhs, number k) { return Vec{lhs} /= k; }
+
+    // IO相关
+    friend std::ostream& operator<<(std::ostream& os, const Vec& vec) {
+        for (int i = 0; i < N - 1; ++i) os << vec[i] << ' ';
+        return os << vec[N - 1];
+    }
+    friend std::istream& operator>>(std::istream& is, Vec& vec) {
+        for (int i = 0; i < N; ++i) is >> vec[i];
+        return is;
+    }
 #pragma endregion
 };
 
@@ -127,6 +186,11 @@ using Vec2 = Vec<2>;
 using Vec3 = Vec<3>;
 // 四元数
 using Vec4 = Vec<4>;
+
+template<class... Args>
+inline auto make_vec(Args&&... args) {
+    return Vec<sizeof...(args)>{number(args)...};
+}
 
 } // namespace mne
 
