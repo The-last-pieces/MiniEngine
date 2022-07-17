c8d2695b4a02c30551352444510a8f56f8172376
MnZn <2776835685@qq.com>
2022-07-17 23:27:03 +0800

    feat: add <TGAImage> class

--- src/view/image.hpp
+++ src/view/image.hpp
@@ -6,9 +6,10 @@
 #define MINI_ENGINE_IMAGE_HPP
 
 #include "color.hpp"
+#include "../math/vec.hpp"
 #include <vector>
 #include <fstream>
-#include <windows.h>
+#include <Windows.h>
 
 #undef min
 #undef max
@@ -91,6 +92,155 @@ public:
 
         delete[] buf;
         return image;
+    }
+};
+
+// 储存tga文件
+class TGAImage {
+    enum Format {
+        GRAYSCALE = 1,
+        RGB       = 3,
+        RGBA      = 4
+    };
+
+    struct TGAColor {
+        std::uint8_t color[4]{}; // blue , green , red , alpha
+        std::uint8_t bytePerPixel{};
+
+        TGAColor() = default;
+
+        TGAColor(std::uint8_t R, std::uint8_t G, std::uint8_t B, std::uint8_t A = 255):
+            color{B, G, R, A}, bytePerPixel(4) {}
+
+        TGAColor(const std::uint8_t* p, int bpp):
+            bytePerPixel(bpp) {
+            for (int i = bpp; i--; color[i] = p[i])
+                ;
+        }
+
+        std::uint8_t& operator[](int i) { return color[i]; }
+
+        Color toColor() const {
+            return Color::fromRGB256(color[2], color[1], color[0]);
+        }
+    };
+
+#pragma pack(push, 1)
+    struct TGAHeader {
+        std::uint8_t  idSize{};
+        std::uint8_t  colorMapType{};
+        std::uint8_t  dataType{};
+        std::uint16_t colorMapOrigin{};
+        std::uint16_t colorMapSize{};
+        std::uint8_t  colorMapDepth{};
+        std::uint16_t xOrigin{};
+        std::uint16_t yOrigin{};
+        std::uint16_t width{};
+        std::uint16_t height{};
+        std::uint8_t  bitsPerPixel{};
+        std::uint8_t  imageDescriptor{};
+    };
+#pragma pack(pop)
+
+private:
+    std::vector<std::uint8_t> data;
+
+    int w{}, h{}, bpp{};
+
+public:
+    TGAColor getPixel(int i, int j) const {
+#ifndef NDEBUG
+        if (i < 0 || i >= h || j < 0 || j >= w)
+            throw std::out_of_range("TGAImage::getPixel");
+#endif
+        return {&data[i * w * bpp + j * bpp], bpp};
+    }
+
+    // 通过进行插值的方式获取颜色信息 Todo
+    Color getPixel(const Vec2& uv) const {
+        auto tmp = getPixel(int(uv.y() * number(h)), int(uv.x() * number(w)));
+        return tmp.toColor();
+    }
+
+public:
+    void loadFromDisk(std::string_view tag_path) {
+        std::ifstream in;
+        in.open(tag_path.data(), std::ios::binary);
+        if (!in.is_open()) {
+            return;
+        }
+        TGAHeader header;
+        in.read(reinterpret_cast<char*>(&header), sizeof(header));
+        if (!in.good()) {
+            return;
+        }
+        w   = header.width;
+        h   = header.height;
+        bpp = header.bitsPerPixel >> 3;
+        if (w <= 0 || h <= 0 || (bpp != GRAYSCALE && bpp != RGB && bpp != RGBA)) {
+            return;
+        }
+        int bSize = bpp * w * h;
+        data.assign(bSize, 0);
+        if (3 == header.dataType || 2 == header.dataType) {
+            in.read(reinterpret_cast<char*>(data.data()), bSize);
+            if (!in.good()) return;
+        } else if (10 == header.dataType || 11 == header.dataType) {
+            if (!loadRleData(in)) return;
+        } else {
+            in.close();
+        }
+        // Todo 处理翻转
+        //        if (!(header.imageDescriptor & 0x20))
+        //            ; //flip_vertically();
+        //        if (header.imageDescriptor & 0x10)
+        //            ; //flip_horizontally();
+        in.close();
+    }
+
+private:
+    bool loadRleData(std::istream& in) {
+        size_t   pixelCount   = w * h;
+        size_t   currentPixel = 0;
+        size_t   currentByte  = 0;
+        TGAColor colorBuffer;
+        do {
+            std::uint8_t chunkHeader = 0;
+            chunkHeader              = in.get();
+            if (!in.good()) {
+                return false;
+            }
+            if (chunkHeader < 128) {
+                chunkHeader++;
+                for (int i = 0; i < chunkHeader; i++) {
+                    in.read(reinterpret_cast<char*>(colorBuffer.color), bpp);
+                    if (!in.good()) {
+                        return false;
+                    }
+                    for (int t = 0; t < bpp; t++)
+                        data[currentByte++] = colorBuffer.color[t];
+                    currentPixel++;
+                    if (currentPixel > pixelCount) {
+                        return false;
+                    }
+                }
+            } else {
+                chunkHeader -= 127;
+                in.read(reinterpret_cast<char*>(colorBuffer.color), bpp);
+                if (!in.good()) {
+                    return false;
+                }
+                for (int i = 0; i < chunkHeader; i++) {
+                    for (int t = 0; t < bpp; t++)
+                        data[currentByte++] = colorBuffer.color[t];
+                    currentPixel++;
+                    if (currentPixel > pixelCount) {
+                        return false;
+                    }
+                }
+            }
+        } while (currentPixel < pixelCount);
+        return true;
     }
 };
 
