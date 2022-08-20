7eb46fc73d07041e1334af5e7e536bd643b59666
MnZn <2776835685@qq.com>
2022-08-20 15:43:07 +0800

    feat: add rt_render test func

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 ﻿#include "view/gui.hpp"
 #include "./engine/render/rs_render.hpp"
+#include "./engine/render/rt_render.hpp"
+#include "./engine/objects/rectangle.hpp"
+#include "./engine/objects/sphere.hpp"
 
 using namespace mne;
 
@@ -53,7 +56,7 @@ public:
     }
 };
 
-int main() {
+void testRsRender() {
     std::string_view paths[][2] = {
         {
             R"(res\boggie\body.obj)",
@@ -79,7 +82,7 @@ int main() {
         model->transform.scale      = {50, 50, 50};
         model->transform.rotate.y() = pi;
         model->transform.rotate.z() = -pi / 2;
-        render->models.push_back(model);
+        render->scene->models.push_back(model);
     };
 
     for (auto [obj, tga] : paths) {
@@ -98,5 +101,98 @@ int main() {
         "MnZn's Graphics Engine",
         SCR_WIDTH, SCR_HEIGHT, render);
     window.show();
+}
+
+void testRtRender() {
+    // 材质
+    //材质
+    //-------------------------------------------------------------
+    auto white = Color::fromRGB256(255, 255, 255);
+
+    auto redDiffuseMat = std::make_shared<MaterialDefault>(
+        Color{}, Color::fromRGB256(255, 0, 0));
+    auto greenDiffuseMat = std::make_shared<MaterialDefault>(
+        Color{}, Color::fromRGB256(0, 255, 0));
+    auto blueDiffuseMat = std::make_shared<MaterialDefault>(
+        Color{}, Color::fromRGB256(0, 0, 255));
+    auto whiteDiffuseMat = std::make_shared<MaterialDefault>(
+        Color{}, Color::fromRGB256(255, 255, 255));
+    auto lightMat = std::make_shared<MaterialDefault>(
+        white * 150, Color{});
+
+    //坐标
+    //-------------------------------------------------------------
+    const int scale = 3;
+
+    Vec3 o  = scale * Vec3{0, -100 + 30, -60}; //球体坐标
+    Vec3 A  = scale * Vec3{-100, 100, 100};    // 矩形 Cornell Box 顶点坐标
+    Vec3 B  = scale * Vec3{-100, -100, 100};
+    Vec3 C  = scale * Vec3{100, -100, 100};
+    Vec3 D  = scale * Vec3{100, 100, 100};
+    Vec3 E  = scale * Vec3{-100, 100, -100};
+    Vec3 F  = scale * Vec3{-100, -100, -100};
+    Vec3 G  = scale * Vec3{100, -100, -100};
+    Vec3 H  = scale * Vec3{100, 100, -100};
+    Vec3 L1 = scale * Vec3{20, 99, 20 - 40}; // 矩形灯光顶点坐标
+    Vec3 L2 = scale * Vec3{-20, 99, 20 - 40};
+    Vec3 L3 = scale * Vec3{-20, 99, -20 - 40};
+    Vec3 L4 = scale * Vec3{20, 99, -20 - 40};
+
+    //物体
+    //-------------------------------------------------------------
+    // 球体
+    auto redSphere = std::make_shared<Sphere>(o, scale * 30.f, blueDiffuseMat);
+    // Cornell Box
+    auto bottomRectangle = std::make_shared<mne::Rectangle>(G, F, B, C, whiteDiffuseMat);
+    auto topRectangle    = std::make_shared<mne::Rectangle>(H, D, A, E, whiteDiffuseMat);
+    auto leftRectangle   = std::make_shared<mne::Rectangle>(A, B, F, E, redDiffuseMat);
+    auto rightRectangle  = std::make_shared<mne::Rectangle>(C, D, H, G, greenDiffuseMat);
+    auto backRectangle   = std::make_shared<mne::Rectangle>(E, F, G, H, whiteDiffuseMat);
+
+    //光源
+    //-------------------------------------------------------------
+    auto lightRectangle = std::make_shared<mne::Rectangle>(L1, L2, L3, L4, lightMat); //面光源
+
+    //场景
+    //-------------------------------------------------------------
+    auto scene = std::make_shared<Scene>();
+    scene->addObject(redSphere);
+    scene->addObject(bottomRectangle);
+    scene->addObject(topRectangle);
+    scene->addObject(leftRectangle);
+    scene->addObject(rightRectangle);
+    scene->addObject(backRectangle);
+    scene->addObject(lightRectangle);
+
+    //相机
+    //-------------------------------------------------------------
+    auto camera = std::make_shared<Camera>();
+
+    camera->eye_pos  = {0, 0, 500};
+    camera->eye_dir  = {0, 0, -1};
+    camera->eye_norm = {0, 1, 0};
+    camera->vh = camera->vw = 800;
+
+    auto render    = std::make_shared<RtRender>();
+    render->scene  = scene;
+    render->camera = camera;
+    render->spp    = 50;
+
+    constexpr bool ui = true;
+    if constexpr (ui) {
+        mne::MainWindow window(
+            "MnZn's Graphics Engine",
+            SCR_WIDTH, SCR_HEIGHT, render);
+        window.show();
+    } else {
+        auto path      = R"(G:\projects\Clion\MiniEngine\out\out.bmp)";
+        *render->image = mne::BMPImage::loadFromDisk(path);
+        render->render();
+        render->image->saveToDisk(path);
+    }
+}
+
+int main() {
+    testRtRender();
     return 0;
 }
