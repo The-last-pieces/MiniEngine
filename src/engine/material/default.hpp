b4e44e4538f018d1c461b191ab6d4c7d2d36f339
MnZn <2776835685@qq.com>
2022-08-20 15:47:47 +0800

    feat: declare default material

---
+++ src/engine/material/default.hpp
@@ -0,0 +1,17 @@
+﻿//
+// Created by IMEI on 2022/8/19.
+//
+
+#ifndef MINI_ENGINE_DEFAULT_HPP
+#define MINI_ENGINE_DEFAULT_HPP
+
+#include "../material/diffuse.hpp"
+
+namespace mne {
+
+// 缺省材质 : 漫反射
+using MaterialDefault = MaterialDiffuse;
+
+} // namespace mne
+
+#endif //MINI_ENGINE_DEFAULT_HPP
