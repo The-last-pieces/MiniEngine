375869a0a26a3731fb0d7ec32ae1df9d0c010537
MnZn <2776835685@qq.com>
2022-08-20 15:48:24 +0800

    feat: declare of <Axis Align Bounding Box>

---
+++ src/engine/accelerator/AABB.hpp
@@ -0,0 +1,25 @@
+﻿//
+// Created by IMEI on 2022/8/18.
+//
+
+#ifndef MINI_ENGINE_AABB_HPP
+#define MINI_ENGINE_AABB_HPP
+
+#include "../math/vec.hpp"
+#include "../data/ray.hpp"
+
+namespace mne {
+
+struct AABB {
+    Vec3 min;
+    Vec3 max;
+
+    // Todo 使用AABB优化射线检测
+    bool intersect(const Ray& ray) const {
+        return true;
+    }
+};
+
+} // namespace mne
+
+#endif //MINI_ENGINE_AABB_HPP
