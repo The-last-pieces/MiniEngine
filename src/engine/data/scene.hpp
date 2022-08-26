a1d39329b15976ca7bee4066deba86c3222231ed
MnZn <2776835685@qq.com>
2022-08-26 09:42:36 +0800

    perf: make the rendering result more realistic

--- src/engine/data/scene.hpp
+++ src/engine/data/scene.hpp
@@ -7,7 +7,7 @@
 
 #include "../store/model.hpp"
 #include "../interface/object.hpp"
-#include "../objects/rectangle.hpp"
+#include "../material/mirror.hpp"
 #include <numeric>
 
 namespace mne {
@@ -21,62 +21,6 @@ public:
         objects.push_back(std::move(object));
     }
 
-public:
-    // 以wr为观测方向 , hit为观测点 , 观测全局光照下的像素结果 . wr wi点乘n>=0
-    Color shade(Vec3 wr, const HitResult& hit) const {
-        constexpr int    times = 8;
-        constexpr number prr   = 1 - 1.f / times; // 轮盘赌概率
-
-        auto* obj = hit.obj;
-        auto& mat = *obj->material;
-
-        if (obj->isLight()) {
-            return mat.emission;
-        }
-
-        Color lr = {};         // 出射光线
-        Vec3  n  = hit.normal; // 法线
-        Vec3  p  = hit.point;  // 观测点
-
-        HitResult hit2;
-
-        /// 对发光object采样
-
-        auto& light = (Rectangle&) (*objects.back());
-
-        LightResult res;
-        light.sampleLight(res);
-        auto [li_l, n_l, p_l] = res;
-
-        Vec3 wi_l = (p_l - p).normalize();
-
-        // 检测是否能被光源照射
-        if (rayCast(Ray{p, wi_l}, hit2) && hit2.obj == &light) {
-            Color  f_r_l = mat.reflect(wi_l, wr, n);
-            number pdf_l = light.PDF();
-
-            lr = lr + li_l.bitMut(f_r_l) * ((n * wi_l) * (n_l * -wi_l) / (p_l - p).norm2() / pdf_l);
-        }
-
-        /// 对不发光的物体进行采样
-
-        // 轮盘赌
-        if (randFloat() > prr) return lr;
-
-        Vec3 wi = mat.sample(wi_l, wr, n); // 随机采样的入射方向
-
-        if (rayCast(Ray{hit.point, wi}, hit2) && !hit2.obj->isLight()) {
-            Color  li  = shade(-wi, hit2);       // 外部光照
-            number dot = n * wi;                 // 夹角
-            Color  f_r = mat.reflect(wi, wr, n); // 反射光
-            number pdf = mat.PDF(wi, n);         // wi的概率密度
-
-            lr = lr + li.bitMut(f_r) * (dot / pdf / prr);
-        }
-
-        return lr;
-    }
-
     // 射线检测 Todo 使用BVH优化
     bool rayCast(const Ray& ray, HitResult& hit) const {
         HitResult temp;
@@ -84,13 +28,78 @@ public:
         number    tick  = std::numeric_limits<number>::max();
 
         for (const auto& ptr : objects) {
-            if (ptr->intersect(ray, temp) && temp.tick > 0.01 && temp.tick < tick) { //
+            if (ptr->intersect(ray, temp) && temp.tick > 0.1 && temp.tick < tick) { //
                 isHit = true;
                 tick  = temp.tick;
                 hit   = temp;
             }
         }
         return isHit;
+    }
+
+private:
+    // Todo 按面积等概率选择光源
+    // Todo 返回pdf
+    const IObject& selectLight() const {
+        return (*objects.back());
+    }
+
+public:
+    // 以-out_dir为观测方向 , hit为观测点 , 观测全局光照下的像素结果 . out_dir*n , in_dir*n >=0
+    Color shade(const Vec3& out_dir, const HitResult& hit, int depth = 0) const {
+        /// 配置 -----------------------------
+        constexpr int    min_dep = 3, max_dep = 10; // 深度限制
+        constexpr number p_rr = 0.8_n;              // 轮盘赌机制
+
+        /// 简写 -----------------------------
+        auto& obj = *(hit.obj);      // 观测点所在的图元
+        auto& mat = *(obj.material); // 观测点的材质
+
+        /// 递归终止条件 ----------------------
+        if (obj.isLight()) return mat.emission.clamp(); // 直接观测到光源
+        if (depth > max_dep) return Color{0, 0, 0};     // 超过最大深度
+
+        /// 直接光照 --------------------------
+        Color L_direct{};
+        auto& light = selectLight(); // 随机选择一个光源
+
+        LightResult ems; // 随机采样
+        light.sampleLight(ems);
+
+        auto l_in     = ems.point - hit.point; // 光线矢量
+        auto l_in_dir = l_in.normalize();      // 光线方向
+
+        HitResult hit2; // 检测是否被遮挡
+        if (rayCast(Ray{hit.point, l_in_dir}, hit2) && hit2.obj == &light) {
+            Color  f_r_l = mat.reflect(l_in_dir, out_dir, hit.normal); // 反射率
+            number pdf_l = light.PDF();                                // 光源采样的pdf
+            number dot   = hit.normal * l_in_dir;                      // 与观测点夹角
+            number dot_l = -(ems.normal * l_in_dir);                   // 与光源夹角
+            Color  le_l  = ems.emission;                               // 直接光照
+
+            L_direct = (le_l * f_r_l) * (dot * dot_l / pdf_l / l_in.norm2());
+        }
+
+        /// 间接光照 --------------------------
+        Color L_indirect{};
+        if (depth > min_dep && randFloat() > p_rr) return L_direct; // 轮盘赌机制
+        Vec3 in_dir = mat.sample(l_in_dir, out_dir, hit.normal);    // 随机采样一个方向
+        // Todo 镜面反射材质需要特殊处理
+        // 间接光照来自物体
+        bool isMirror = dynamic_cast<MaterialMirror*>(&mat);
+        if (rayCast(Ray{hit.point, in_dir}, hit2) && (isMirror || !hit2.obj->isLight())) {
+            Color  f_r = mat.reflect(in_dir, out_dir, hit.normal); // 反射率
+            number pdf = mat.PDF(in_dir, hit.normal);              // 采样的pdf
+            number dot = hit.normal * in_dir;                      // 与观测点夹角
+            Color  le  = shade(-in_dir, hit2, depth + 1);          // 递归计算间接光照
+
+            L_indirect = (le * f_r) * (dot / pdf / p_rr);
+        }
+
+        /// 返回结果 --------------------------
+        //  镜面材质只需要间接光照
+        if (isMirror) return L_indirect;
+        return (L_direct + L_indirect);
     }
 };
 
