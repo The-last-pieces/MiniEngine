aa90a5257d84fbd2d0f33fe1526475ceb6576f40
MnZn <2776835685@qq.com>
2022-08-26 09:32:57 +0800

    feat: add <clamp> api , ?= operators

--- src/engine/data/color.hpp
+++ src/engine/data/color.hpp
@@ -15,11 +15,14 @@
 namespace mne {
 struct Color {
     // 范围为[0,1]
-    float r, g, b; // Todo 是否考虑alpha通道?
+    number r, g, b; // Todo 是否考虑alpha通道?
 
     // 约束在[0,1]范围
-    Color clamp() const {
-        return {mne::clamp(0.0f, r, 1.0f), mne::clamp(0.0f, g, 1.0f), mne::clamp(0.0f, b, 1.0f)};
+    constexpr Color clamp() const {
+        return {
+            mne::clamp(0_n, r, 1_n),
+            mne::clamp(0_n, g, 1_n),
+            mne::clamp(0_n, b, 1_n)};
     }
 
     // 颜色混合
@@ -28,28 +31,44 @@ struct Color {
     }
 
     // 数乘
-    friend constexpr Color operator*(const Color& lhs, float mut) {
+    friend constexpr Color operator*(const Color& lhs, number mut) {
         return {lhs.r * mut, lhs.g * mut, lhs.b * mut};
     }
 
-    friend constexpr Color operator*(float mut, const Color& rhs) {
-        return rhs * mut;
+    friend constexpr Color operator*(number mut, const Color& rhs) {
+        return {rhs.r * mut, rhs.g * mut, rhs.b * mut};
     }
 
     // 按位乘
-    Color bitMut(Color oth) const {
-        return {r * oth.r, g * oth.g, b * oth.b};
+    friend constexpr Color operator*(const Color& lhs, const Color& rhs) {
+        return {lhs.r * rhs.r, lhs.g * rhs.g, lhs.b * rhs.b};
     }
 
     // 数除
-    friend constexpr Color operator/(const Color& lhs, float mut) {
+    friend constexpr Color operator/(const Color& lhs, number mut) {
         return {lhs.r / mut, lhs.g / mut, lhs.b / mut};
+    }
+
+    // 赋值
+    Color& operator+=(const Color& rhs) {
+        r += rhs.r, g += rhs.g, b += rhs.b;
+        return *this;
+    }
+
+    Color& operator*=(number mut) {
+        r *= mut, g *= mut, b *= mut;
+        return *this;
+    }
+
+    Color& operator/=(number mut) {
+        r /= mut, g /= mut, b /= mut;
+        return *this;
     }
 
     // 将[0,256)映射到[0,1]
     static constexpr Color fromRGB256(std::uint8_t r, std::uint8_t g, std::uint8_t b) {
         constexpr int div = 255;
-        return {float(r) / div, float(g) / div, float(b) / div};
+        return {number(r) / div, number(g) / div, number(b) / div};
     }
 };
 } // namespace mne
