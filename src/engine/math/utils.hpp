c2e6ced718f62b0027d2a3500818bb0d59328bd4
MnZn <2776835685@qq.com>
2022-08-26 15:21:46 +0800

    refactor: move MatUtils to utils.hpp

--- src/engine/math/utils.hpp
+++ src/engine/math/utils.hpp
@@ -7,6 +7,8 @@
 
 #include <random>
 #include <ctime>
+#include "../math/vec.hpp"
+#include "../math/mat.hpp"
 
 namespace mne {
 
@@ -100,6 +102,170 @@ public:
         // Todo 转换到世界坐标系意义注释
         return toWorld(make_vec(x, y, z), n);
     }
+};
+
+// 矩阵相关的工厂函数
+class MatUtils {
+public:
+#pragma region 杂项函数
+    // 单位矩阵
+    template<int N>
+    constexpr static Mat<N, N> identity() {
+        Mat<N, N> m;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                m.at(i, j) = i == j ? 1.0f : 0.0f;
+            }
+        }
+        return m;
+    }
+
+    // 3维向量转反对称矩阵
+    constexpr static Mat33 anti(const Vec3& input) {
+        auto [x, y, z] = input.data;
+        return {
+            {0, -z, y},
+            {z, 0, -x},
+            {-y, x, 0}};
+    }
+
+    // 合并变换,如果需要连续执行A1~An,则返回An*...*A1
+    template<class T, class... Args>
+    constexpr static T merge(const T& first, const Args&... args) {
+        if constexpr (sizeof...(args) == 0) {
+            return first;
+        } else {
+            return merge(args...) * first;
+        }
+    }
+
+#pragma endregion
+public:
+#pragma region 旋转矩阵3x3
+    // 绕X轴转
+    static Mat33 rotateX(number angle) {
+        number c = std::cos(angle);
+        number s = std::sin(angle);
+        return Mat33{
+            {1, 0, 0},
+            {0, c, -s},
+            {0, s, c}};
+    }
+
+    // 绕Y轴转
+    static Mat33 rotateY(number angle) {
+        number c = std::cos(angle);
+        number s = std::sin(angle);
+        return Mat33{
+            {c, 0, s},
+            {0, 1, 0},
+            {-s, 0, c}};
+    }
+
+    // 绕Z轴转
+    static Mat33 rotateZ(number angle) {
+        number c = std::cos(angle);
+        number s = std::sin(angle);
+        return Mat33{
+            {c, -s, 0},
+            {s, c, 0},
+            {0, 0, 1}};
+    }
+
+    // 绕(过原点的)任意轴旋转,右手拇指朝向k,四指方向为旋转方向
+    static Mat33 rotate(const Vec3& k, number angle) {
+        // R = I + sin(θ) * K + (1 - cos(θ)) * K^2
+        number c = std::cos(angle);
+        number s = std::sin(angle);
+        Mat33  K = anti(k.normalize());
+        return identity<3>() + K * s + (K * K) * (1 - c);
+    }
+
+    // 绕任意点和任意轴旋转
+    static Mat44 rotateAt(const Vec3& p, const Vec3& k, number angle) {
+        return merge(translate(-p), rotate(k, angle).as4(), translate(p));
+    }
+
+    // 从某个方向旋转到+Z轴方向
+    static Mat33 rotateToZ(const Vec3& dir) {
+        // 无视x轴,绕x轴旋转到z
+        auto x_mat = rotateX(dir.pick<1, 2>().rotate({0, 1}));
+        // 无视y轴,绕y轴旋转到z
+        auto y_mat = rotateY((x_mat * dir).pick<0, 2>().rotate({0, 1}));
+        return merge(x_mat, y_mat);
+    }
+
+    // 从source旋转到dest方向
+    static Mat33 rotateFT(const Vec3& s, const Vec3& d) {
+        // s->z -(d->z)
+        return merge(rotateToZ(s), rotateToZ(d).invert());
+    }
+
+#pragma endregion
+public:
+#pragma region 其他变换
+    // 平移变换Ax=x+v
+    static Mat44 translate(const Vec3& v) {
+        return Mat44{
+            {1, 0, 0, v.x()},
+            {0, 1, 0, v.y()},
+            {0, 0, 1, v.z()},
+            {0, 0, 0, 1}};
+    }
+
+    // 缩放变换(坐标轴方向)
+    static Mat33 scaleXYZ(const Vec3& sz) {
+        return Mat33{
+            {sz.x(), 0, 0},
+            {0, sz.y(), 0},
+            {0, 0, sz.z()}};
+    }
+
+    // 缩放变换(原点,任意轴方向)
+    static Mat33 scale(const Vec3& dir, const Vec3& sz) {
+        // d->z *sz -(d->z)
+        auto dz = rotateToZ(dir);
+        return merge(dz, scaleXYZ(sz), dz.invert());
+    }
+
+    /**
+     * @brief 透视变换矩阵
+     * @param angle 视锥上下面之间的夹角
+     * @param aspect viewPort的宽高比
+     * @param n near面的距离
+     * @param f far面的距离
+     * @return
+     */
+    static inline Mat44 perspective(number angle, number aspect, number n, number f) {
+        number q = 1.0f / tan(0.5f * angle);
+        number A = q / aspect;
+        number B = (n + f) / (n - f);
+        number C = (2.0f * n * f) / (n - f);
+
+        return {
+            make_vec(A, 0, 0, 0),
+            make_vec(0, q, 0, 0),
+            make_vec(0, 0, -B, -1),
+            make_vec(0, 0, -C, 0)};
+    }
+
+    /**
+     * @brief 正交变换矩阵 Todo
+     */
+    static inline Mat44 ortho(number left, number right, number bottom, number top, number zNear, number zFar) {
+        //        return {
+        //            make_vec(2.0f / (right - left), 0.0f, 0.0f, 0.0f),
+        //            make_vec(0.0f, 2.0f / (top - bottom), 0.0f, 0.0f),
+        //            make_vec(0.0f, 0.0f, -2.0f / (zFar - zNear), 0.0f),
+        //            make_vec(-(right + left) / (right - left), -(top + bottom) / (top - bottom), -(zFar + zNear) / (zFar - zNear), 1.0f)};
+        number width = right - left, height = top - bottom, depth = zFar - zNear;
+        return {
+            make_vec(2 / width, 0, 0, -(right + left) / width),
+            make_vec(0, 2 / height, 0, -(top + bottom) / height),
+            make_vec(0, 0, -2 / depth, -(zFar + zNear) / depth),
+            make_vec(0, 0, 0, 1)};
+    }
+#pragma endregion
 };
 
 } // namespace mne
