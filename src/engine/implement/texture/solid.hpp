643c0c6b9b3714c367709fad1c654943e63b2762
MnZn <2776835685@qq.com>
2022-09-02 16:49:45 +0800

    feat: add <Solid> texture

---
+++ src/engine/implement/texture/solid.hpp
@@ -0,0 +1,27 @@
+﻿//
+// Created by IMEI on 2022/8/31.
+//
+
+#ifndef MINI_ENGINE_SOLID_HPP
+#define MINI_ENGINE_SOLID_HPP
+
+#include "../../interface/texture.hpp"
+
+namespace mne {
+
+// 纯色纹理
+class TextureSolid: public ITexture {
+    Color emission;
+
+public:
+    TextureSolid(const Color& _emission):
+        emission(_emission) {}
+
+    Color value(const Vec2& uv) const override {
+        return emission;
+    }
+};
+
+} // namespace mne
+
+#endif //MINI_ENGINE_SOLID_HPP
