f3343447bc4bf90532c65d5562aeee3f5cb9a790
MnZn <2776835685@qq.com>
2022-09-02 15:40:48 +0800

    feat: add interface <ITexture>

---
+++ src/engine/interface/texture.hpp
@@ -0,0 +1,21 @@
+﻿//
+// Created by IMEI on 2022/8/16.
+//
+
+#ifndef MINI_ENGINE_TEXTURE_HPP
+#define MINI_ENGINE_TEXTURE_HPP
+
+#include "../data/color.hpp"
+
+namespace mne {
+
+// 纹理
+class ITexture {
+public:
+    // u,v为纹理坐标,范围为[0,1]
+    virtual Color value(const Vec2& uv) const = 0;
+};
+
+} // namespace mne
+
+#endif //MINI_ENGINE_TEXTURE_HPP
