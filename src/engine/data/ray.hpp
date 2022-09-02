7c5cbd3208a3107ce533c779597905545393944f
MnZn <2776835685@qq.com>
2022-09-02 16:45:15 +0800

    perf: update hit check logic

--- src/engine/data/ray.hpp
+++ src/engine/data/ray.hpp
@@ -29,33 +29,42 @@ struct Ray {
 class IObject;
 
 // 射线碰撞点信息
-struct HitResult {
-    Vec3 point;  // 点坐标
-    Vec3 normal; // 面法线
-    Vec2 uv;     // 纹理坐标
-    bool back{}; // 是否位于背面
+class HitResult {
+public:
+    Vec3 point;     // 点坐标
+    Vec3 normal;    // 面法线
+    Vec2 uv;        // 纹理坐标
+    bool back{};    // 是否位于背面
+    bool success{}; // 是否成功碰撞
 
     const IObject* obj{}; // 碰撞到的物体
+private:
+    number tick{}; // point = pos + tick * dir
+    number min_tick = 1_n, max_tick = inf;
 
+public:
     // outSide为朝外的法线
     void setNormal(Vec3 outSide, const Ray& ray) {
         back   = (outSide * ray.dir) > 0;
         normal = back ? -outSide : outSide;
     }
 
-    void setTick(number val) {
+    bool setTick(number val) {
         if (val > min_tick && val < max_tick) {
-            tick = max_tick = val;
+            return tick = max_tick = val, success = true;
+        } else {
+            return false;
         }
+    }
+
+    // 假定要求v1 < v2
+    bool setTick(number v1, number v2) {
+        return setTick(v1) || setTick(v2);
     }
 
     Vec3 getPoint(const Ray& ray) const {
         return ray.at(tick);
     }
-
-private:
-    number tick{}; // point = pos + tick * dir
-    number min_tick = 1_n, max_tick = inf;
 };
 
 } // namespace mne
