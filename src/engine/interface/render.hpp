c70b074c0db7e1c444438d94fd40e3215818a85c
MnZn <2776835685@qq.com>
2022-09-03 19:47:31 +0800

    refactor: update <IRender> structural

--- src/engine/interface/render.hpp
+++ src/engine/interface/render.hpp
@@ -13,11 +13,16 @@ namespace mne {
 // 渲染器接口,输入摄像机+光源+模型信息,输出图片
 class IRender {
 public:
-    std::shared_ptr<Camera> camera = std::make_shared<Camera>(); // 摄像机
-    std::shared_ptr<Scene>  scene  = std::make_shared<Scene>();  // 场景
-    std::shared_ptr<Image>  image  = std::make_shared<Image>();  // 输出
+    std::shared_ptr<Camera> camera; // 摄像机
 
-    int spp = 1; // 采样率(sample per pixel)
+    std::shared_ptr<Image> image = std::make_shared<Image>(); // 输出的图片
+    std::shared_ptr<Scene> scene = std::make_shared<Scene>(); // 输入的场景
+
+    // 背景色/环境光
+    Color background = Color::fromRGB256(255, 255, 255) * 0.5_n;
+
+    // 采样率(sample per pixel)
+    int spp = 1;
 
 public:
     virtual void render() = 0;
