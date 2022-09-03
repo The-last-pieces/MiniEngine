4869596f131c61ce3f5bae01d24afe3967793b3b
MnZn <2776835685@qq.com>
2022-09-03 20:58:30 +0800

    refactor: update render test logic

--- src/main.cpp
+++ src/main.cpp
@@ -12,51 +12,66 @@ using namespace mne;
 const int SCR_WIDTH  = 720;
 const int SCR_HEIGHT = 720;
 
-// 转发纹理的shader
-class TextureShader: public IShader {
-    Model& model;
+void show(bool ui, std::shared_ptr<IRender> render) {
+    if (ui) {
+        mne::MainWindow window(
+            "MnZn's Graphics Engine",
+            SCR_WIDTH, SCR_HEIGHT, render);
+        window.show();
+    } else {
+        // Todo 自动递增
+        auto path = R"(..\demo\out.png)";
+        render->render();
+        render->image->saveToDisk(path);
+    }
+}
 
-public:
-    TextureShader(Model& model):
-        model(model) {
-    }
+void testRsRender() {
+    // 转发纹理的shader
+    class TextureShader: public IShader {
+        Model& model;
 
-    void vertex(
-        Vec3&        gl_Position,
-        const Vec2&  gl_TexCoord,
-        const Mat44& gl_Transform,
-        Color&       gl_Color) final {
-        gl_Position = gl_Transform * gl_Position;
-    }
-    void fragment(
-        const Vec3& gl_FragCoord,
-        const Vec2& gl_TexCoord,
-        Color&      gl_FragColor,
-        bool&       gl_Discard) final {
-        gl_FragColor = model.colorTexture->value(gl_TexCoord);
-    }
-};
+    public:
+        TextureShader(Model& model):
+            model(model) {
+        }
 
-// 使用顶点着色的shader
-class VertexShader: public IShader {
-public:
-    void vertex(
-        Vec3&        gl_Position,
-        const Vec2&  gl_TexCoord,
-        const Mat44& gl_Transform,
-        Color&       gl_Color) final {
-        gl_Position = gl_Transform * gl_Position;
-        gl_Color    = {gl_TexCoord.x(), gl_TexCoord.y(), 0};
-        // Color::fromRGB256(randInt(256), randInt(256), randInt(256));
-    }
-    void fragment(
-        const Vec3& gl_FragCoord,
-        const Vec2& gl_TexCoord,
-        Color&      gl_FragColor,
-        bool&       gl_Discard) final {
-        gl_Discard = true;
-    }
-};
+        void vertex(
+            Vec3&        gl_Position,
+            const Vec2&  gl_TexCoord,
+            const Mat44& gl_Transform,
+            Color&       gl_Color) final {
+            gl_Position = gl_Transform * gl_Position;
+        }
+        void fragment(
+            const Vec3& gl_FragCoord,
+            const Vec2& gl_TexCoord,
+            Color&      gl_FragColor,
+            bool&       gl_Discard) final {
+            gl_FragColor = model.colorTexture->value(gl_TexCoord);
+        }
+    };
+
+    // 使用顶点着色的shader
+    class VertexShader: public IShader {
+    public:
+        void vertex(
+            Vec3&        gl_Position,
+            const Vec2&  gl_TexCoord,
+            const Mat44& gl_Transform,
+            Color&       gl_Color) final {
+            gl_Position = gl_Transform * gl_Position;
+            gl_Color    = {gl_TexCoord.x(), gl_TexCoord.y(), 0};
+            // Color::fromRGB256(randInt(256), randInt(256), randInt(256));
+        }
+        void fragment(
+            const Vec3& gl_FragCoord,
+            const Vec2& gl_TexCoord,
+            Color&      gl_FragColor,
+            bool&       gl_Discard) final {
+            gl_Discard = true;
+        }
+    };
 
 void testRsRender() {
     std::string paths[][2] = {
@@ -81,7 +96,7 @@ void testRsRender() {
     auto render = std::make_shared<RsRender>();
 
     auto add_model = [&](std::shared_ptr<Model>& model) {
-        model->transform.scale = {50, 50, 50};
+        // model->transform.rotate.y() = pi * 0.5 * 7 / 6;
         render->scene->models.push_back(model);
     };
 
@@ -94,14 +109,22 @@ void testRsRender() {
         add_model(model);
     }
 
-    auto model    = std::make_shared<Model>(R"(art\floor.obj)");
+    auto model    = std::make_shared<Model>(R"(art\model\floor.obj)");
     model->shader = std::make_shared<VertexShader>();
     add_model(model);
 
-    mne::MainWindow window(
-        "MnZn's Graphics Engine",
-        SCR_WIDTH, SCR_HEIGHT, render);
-    window.show();
+    auto camera = std::make_shared<Camera>(
+        make_vec(0, 5, 30),
+        make_vec(0, 0, 0),
+        SCR_WIDTH, SCR_HEIGHT,
+        MathUtils::deg2rad(50),
+        0_n);
+
+    render->camera = camera;
+
+    if (false) // 不使用ui
+        return show(false, render);
+    return show(true, render);
 }
 
 void testRtRender() {
@@ -201,32 +224,21 @@ void testRtRender() {
 
     //相机
     //-------------------------------------------------------------
-    auto camera = std::make_shared<RtCamera>(
-        make_vec(0, 0, 380),
+    auto camera = std::make_shared<Camera>(
+        make_vec(0, 0, 380), // make_vec(99, 99, 99)
         make_vec(0, 0, 0),
-        make_vec(0, 1, 0),
+        SCR_WIDTH, SCR_HEIGHT,
         number(atan2(100, 380 - 100) * 2),
-        number(SCR_WIDTH) / SCR_HEIGHT);
+        0_n);
 
-    camera->setViewPort(SCR_WIDTH, SCR_HEIGHT);
+    auto render    = std::make_shared<RtRender>();
+    render->scene  = scene;
+    render->camera = camera;
+    render->spp    = 50;
 
-    auto render     = std::make_shared<RtRender>();
-    render->scene   = scene;
-    render->camera2 = camera;
-    render->spp     = 100;
-
-    constexpr bool ui = true;
-    if constexpr (ui) {
-        mne::MainWindow window(
-            "MnZn's Graphics Engine",
-            SCR_WIDTH, SCR_HEIGHT, render);
-        window.show();
-    } else {
-        // Todo 自动递增
-        auto path = R"(..\demo\out.png)";
-        render->render();
-        render->image->saveToDisk(path);
-    }
+    //if (false) // 不使用ui
+    return show(false, render);
+    return show(true, render);
 }
 
 int main() {
