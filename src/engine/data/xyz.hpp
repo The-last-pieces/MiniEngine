a10bbab21fbccb65bb5cd02b4b15a575d8396715
MnZn <2776835685@qq.com>
2022-09-08 12:46:50 +0800

    refactor: remove unused api

--- src/engine/data/xyz.hpp
+++ src/engine/data/xyz.hpp
@@ -62,52 +62,6 @@ public:
     }
 
 public:
-    // 返回满足z轴方向要求的任意标架
-    static XYZ randXYZ(const Vec3& n) {
-        Vec3 z      = n.normalize();
-        Vec2 map_xz = z.pick<0, 2>(), map_xy = z.pick<0, 1>();
-        if (map_xz.length() < 0.5_n) {
-            // z和xz平面的夹角
-            number phi = z.inner(make_vec(map_xz.x(), 0, map_xz.y()));
-            if (z.y() < 0) phi = -phi;
-            return XYZ().rotate(make_vec(0, phi));
-        } else {
-            // z和xy平面的夹角
-            number phi = z.inner(make_vec(map_xy.x(), map_xy.y(), 0));
-            if (z.z() < 0) phi = -phi;
-            return XYZ().rotate(make_vec(0, phi));
-        }
-    }
-
-public:
-    // 根据标架反推theta_phi
-    Vec2 getThetaPhi() const {
-        if (z.pick<0, 2>().length() > 0.5_n) {
-            // z在xz平面的投影,必须够大才能保证theta的精度
-            Vec2 map_z = z.pick<0, 2>();
-            // (0,0,1)要旋转多少度才能追上map_z
-            number theta = make_vec(0, 1).rotate(map_z);
-            // z和map_z的夹角
-            number phi = z.inner(make_vec(map_z.x(), 0, map_z.y()));
-            if (z.y() < 0) phi *= -1_n;
-            return {theta, phi};
-        } else {
-            // x在xz平面的投影,在此if分支下必然足够大
-            Vec2 map_x = x.pick<0, 2>();
-            // (1,0,0)要旋转多少度才能追上map_x
-            number theta = make_vec(1, 0).rotate(map_x);
-            // x和map_z的夹角
-            number phi = x.inner(make_vec(map_x.x(), 0, map_x.y()));
-            if (x.y() < 0) phi *= -1_n;
-            return {theta, phi};
-        }
-    }
-
-    // 根据标架反推xyz
-    Vec3 getXYZ() const {
-        return MatUtils::angle2xyz(getThetaPhi());
-    }
-
     // 获取原始方向
     std::tuple<Vec3, Vec3, Vec3> getRawDir() const { return {x, y, z}; }
 
